7cc6b7c3c5d3b1de72dff8d981b73b90288f1e63
ghwa <lukling0524@gmail.com>
2025-02-04 21:04:45 +0900

    feat: nuxt config 설정, temp style  반영

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'url';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	ssr: true,
+	experimental: { appManifest: false },
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	alias: {
+		'@': fileURLToPath(new URL('./', import.meta.url)),
+		'@css': fileURLToPath(new URL('./assets/scss', import.meta.url)),
+	},
+	css: ['@css/style.scss'],
+	vite: {
+		build: {
+			cssMinify: true,
+		},
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: '@use "@css/abstracts" as *;',
+				},
+			},
+		},
+	},
+});
